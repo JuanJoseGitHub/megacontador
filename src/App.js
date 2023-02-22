ec7cf111d25f062c77758459a93dd405ca174986
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-22 19:35:28 +0100

    3 botóns

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ function App() {
     setvalorActual(--valorActual)
   }
 
+  function manexadorClickReset()  {
+    setvalorActual(0)
+  }
+
   return (
     <>
       <h1>{valorActual}</h1>
       <button onClick={manexadorClickAumentar}>+</button>
       <button onClick={manexadorClickDisminuir}>-</button>
+      <button onClick={manexadorClickReset}>Reset</button>
     </>   
   );
 }
