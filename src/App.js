95d14ed67c2de67d69b52a24e122f17780ec8c5d
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-22 18:07:55 +0100

    Inicio do megacontador

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let valorActual=0
+
+  return (
+    <>
+      <h1>{valorActual}</h1>
+      <button>Contador</button>
+    </>   
+  );
+}
+
+export default App;
