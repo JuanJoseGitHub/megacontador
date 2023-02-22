cb911f718cce6f7fd31119fbb47c1113895be364
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-22 19:31:50 +0100

    2botóns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  let valorActual=0
+
+  let [valorActual, setvalorActual] = useState(0);
+
+  function manexadorClickAumentar()  {
+    setvalorActual(++valorActual)
+  }
+
+  function manexadorClickDisminuir()  {
+    setvalorActual(--valorActual)
+  }
 
   return (
     <>
       <h1>{valorActual}</h1>
-      <button>Contador</button>
+      <button onClick={manexadorClickAumentar}>+</button>
+      <button onClick={manexadorClickDisminuir}>-</button>
     </>   
   );
 }
